4ec6f74eeacdfa12b9747c013278eb54d70193f0
yuliia levytska <lupin773311asd@gmail.com>
2025-04-20 22:36:32 +0100

    fjsf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { nanoid } from 'nanoid';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import SearchBox from './SearchBox/SearchBox';
